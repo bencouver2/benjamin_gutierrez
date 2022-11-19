5e33e08b63bed885fea438001500aec99d0b4526
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 23:16:02 +0000

    Adding tarfile member sanitization to extractall()

--- cifar100/cifar-100.py
+++ cifar100/cifar-100.py
@@ -54,7 +54,29 @@ def download_dataset():
     urllib.request.urlretrieve(url, './cifar-100-python.tar.gz')  
     target_folder = '.'
     with tarfile.open("./cifar-100-python.tar.gz") as tar:
-      tar.extractall(target_folder)
+      
+      import os
+      
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tar, target_folder)
 
 """ 
 The following are standard preparation for categorical Data.  
